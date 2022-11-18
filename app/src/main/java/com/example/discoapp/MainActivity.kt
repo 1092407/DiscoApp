b21173a9d3bc53e28a55653efc89f07fe4cfd32d
Filippo <s1095256@studenti.univpm.it>
2022-11-18 17:20:34 +0100

    risolto problema temoplate

--- app/src/main/java/com/example/discoapp/MainActivity.kt
+++ app/src/main/java/com/example/discoapp/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.discoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.discoapp.authentication.LoginActivity
+
+import com.example.discoapp.authentication.RegisterActivity
+import com.example.discoapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var user: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        user = FirebaseAuth.getInstance()
+        if (user.currentUser != null){
+            user.currentUser?.let {
+
+            }
+        }
+
+        binding.logout.setOnClickListener{
+            user.signOut()
+            startActivity(
+                Intent(
+                    this,
+                    LoginActivity::class.java
+                )
+            )
+
+            finish()
+        }
+
     }
 }
