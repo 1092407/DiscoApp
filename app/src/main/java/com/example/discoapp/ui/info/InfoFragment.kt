a58f9800eb93d17c650a71bfea794548ce6d1c01
giorg <s1092407@studenti.univpm.it>
2022-11-19 15:03:08 +0100

    menu non collegato ad altri fragment,vista home ok su mio cell

--- app/src/main/java/com/example/discoapp/ui/info/InfoFragment.kt
+++ app/src/main/java/com/example/discoapp/ui/info/InfoFragment.kt
@@ -1,5 +1,63 @@
 package com.example.discoapp.ui.info
 
+
+
+
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.discoapp.databinding.FragmentHomeBinding
+import com.example.discoapp.databinding.FragmentInfoBinding
+
+
+class InfoFragment : Fragment() {
+
+    private var _binding: FragmentInfoBinding? = null
+
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+
+        _binding = FragmentInfoBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        /*  commentata perche ho tolto il texthome dal layout predefinito
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        */
+
+
+
+        return root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
+
+//questo era prima per vedere se camniando andava
+
+/*
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -38,3 +96,6 @@ class InfoFragment : Fragment() {
 
 
 }
+
+
+ */
